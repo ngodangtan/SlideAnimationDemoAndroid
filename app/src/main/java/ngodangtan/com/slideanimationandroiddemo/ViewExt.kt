228e02bcebb9e51082aff25470cc8088145d3a2c
Ngo Dang Tan <ngodangtan1994@gmail.com>
2020-12-28 17:05:08 +0700

    first commit

---
+++ app/src/main/java/ngodangtan/com/slideanimationandroiddemo/ViewExt.kt
@@ -0,0 +1,14 @@
+package ngodangtan.com.slideanimationandroiddemo
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+
+fun View.slideUp(animTime: Long, startOffset: Long){
+    val slideUp = AnimationUtils.loadAnimation(context, R.anim.slide_up).apply {
+        duration = animTime
+        interpolator = FastOutSlowInInterpolator()
+        this.startOffset = startOffset
+
+    }
+    startAnimation(slideUp)
+}
